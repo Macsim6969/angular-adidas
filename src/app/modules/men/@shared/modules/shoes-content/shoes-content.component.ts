79e6ec85ac819684f00d55baabb9e28958bf4273
Max Belousov <macss.belousov699@gmail.com>
2024-01-21 00:13:31 +0200

    added style

--- src/app/modules/men/@shared/modules/shoes-content/shoes-content.component.ts
+++ src/app/modules/men/@shared/modules/shoes-content/shoes-content.component.ts
@@ -3,7 +3,6 @@ import {StoreInterface} from "../../../../../store/model/store.model";
 import {Store} from "@ngrx/store";
 import {storeSelectorShoesData} from "../../../../../store/selectors/store.selectors";
 import {ActivatedRoute, Params} from "@angular/router";
-import {interval, take, timer} from "rxjs";
 
 @Component({
   selector: 'app-shoes-content',
@@ -14,12 +13,12 @@ export class ShoesContentComponent implements OnInit {
   public contentItem: any;
   public choiceColorShoes: number = 0;
   public isMorePhoto: boolean = false;
-  public loading: boolean = false
 
   public sizes: number[] = [
     6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12
   ]
 
+  public titleContentLeft: ['Descriptions', 'Details'] = ['Descriptions', 'Details']
   constructor(
     private store: Store<{ store: StoreInterface }>,
     private route: ActivatedRoute
@@ -29,20 +28,17 @@ export class ShoesContentComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       const targetId = params['id'].split('_').slice(1).map((word) => word.toUpperCase()).join(' ')
-      this.store.select(storeSelectorShoesData).subscribe((data) => {
+      this.store.select(storeSelectorShoesData).subscribe((data: any[]) => {
         if (data.length > 0) {
           this.contentItem = data.find((item: any) => item.name === targetId);
           console.log(this.contentItem)
+
         }
       })
     })
   }
 
   public choiceColor(index: number) {
-    this.loading = true;
     this.choiceColorShoes = index;
-    timer(2000).pipe(take(1)).subscribe(() => {
-      this.loading = false;
-    })
   }
 }
