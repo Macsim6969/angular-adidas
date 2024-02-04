579d88cf7d33fb761c9cb91cdd7965adb2d4f0fe
Max Belousov <macss.belousov699@gmail.com>
2024-02-04 15:59:18 +0200

    finished update search header

--- src/app/modules/header/@shared/components/options/options.component.ts
+++ src/app/modules/header/@shared/components/options/options.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {MatIconService} from "../../../../../services/matIcon.service";
 import {HeaderService} from "../../services/header.service";
 import {StoreInterface} from "../../../../../store/model/store.model";
 import {Store} from "@ngrx/store";
 import {storeSelectorClothesData} from "../../../../../store/selectors/store.selectors";
 import {iterator} from "rxjs/internal/symbol/iterator";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-options',
@@ -16,10 +17,27 @@ export class OptionsComponent implements OnInit {
   isDropdown: boolean;
   searchItem: any
   searchText: string
+
   constructor(
     private matIcon: MatIconService,
-    private store: Store<{store: StoreInterface}>
-  ) {}
+    private store: Store<{ store: StoreInterface }>,
+    private router: Router
+  ) {
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    const targetElement = event.target as HTMLElement
+    if (document.querySelector('.search-list') as HTMLElement) {
+
+      if (!targetElement.closest('.search-list')) {
+        document.querySelectorAll('.search-list').forEach((e) => {
+          e.classList.add('hide')
+          this.searchText = '';
+        })
+      }
+    }
+  }
 
   ngOnInit() {
     this.store.select(storeSelectorClothesData).subscribe((data) => {
@@ -31,12 +49,18 @@ export class OptionsComponent implements OnInit {
 
   }
 
+  public openClothes(route: string, name: string) {
+    const newRoute = name.replace(/ /g, '_').toLowerCase();
+    this.router.navigate(['/men/' + route+'/' +newRoute], {queryParamsHandling: 'merge'}).then();
+    this.searchText = '';
+  }
+
   toggleTooltip() {
     this.isDropdown = !this.isDropdown;
   };
 
-  closeDropdown(event){
-  this.isDropdown = event;
+  closeDropdown(event) {
+    this.isDropdown = event;
   }
 
   protected readonly iterator = iterator;
