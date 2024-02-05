830e5510f7a234eb08b22c82987773244e34a0bb
Max Belousov <macss.belousov699@gmail.com>
2024-02-05 22:17:18 +0200

    add some logic for search cards

--- src/app/modules/header/@shared/components/options/options.component.ts
+++ src/app/modules/header/@shared/components/options/options.component.ts
@@ -15,25 +15,26 @@ import {Router} from "@angular/router";
 export class OptionsComponent implements OnInit {
 
   isDropdown: boolean;
-  searchItem: any
-  searchText: string
-
+  searchItem: any;
+  searchItemCategory: any
+  searchListCategory: any
+  searchText: string;
+  menuAll: boolean;
+  menuCategory: boolean
   constructor(
     private matIcon: MatIconService,
     private store: Store<{ store: StoreInterface }>,
     private router: Router
-  ) {
-  }
+  ) {}
 
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: Event): void {
     const targetElement = event.target as HTMLElement
     if (document.querySelector('.search-list') as HTMLElement) {
-
       if (!targetElement.closest('.search-list')) {
         document.querySelectorAll('.search-list').forEach((e) => {
-          e.classList.add('hide')
-          this.searchText = '';
+          //e.classList.add('hide')
+          //this.searchText = '';
         })
       }
     }
@@ -43,12 +44,38 @@ export class OptionsComponent implements OnInit {
     this.store.select(storeSelectorClothesData).subscribe((data) => {
       if (data && data['hoodies']) {
         this.searchItem = [...data['hoodies'], ...data['pants'], ...data['shoes'], ...data['valentines']];
-        console.log(this.searchItem);
+        this.searchItemCategory = this.searchItem;
+        this.searchItemCategory = Array.from(new Set([...data['hoodies'], ...data['pants'], ...data['shoes'], ...data['valentines']]?.map(item => item.category)))
       }
     });
 
   }
 
+  choiceMenuSearch(active: 'all' | 'category'){
+    if(active === 'all'){
+      this.menuAll = true;
+      this.menuCategory = false;
+      this.searchListCategory = this.searchItem
+    }
+    if(active === 'category'){
+      this.menuCategory = true;
+      this.menuAll = false ;
+    }
+  }
+
+  backList(){
+    this.menuAll = false;
+    this.menuCategory = false;
+  }
+
+  openSearchCategory(id: string){
+    this.searchListCategory = this.searchItem.filter(data => {
+      return data.category === id;
+    });
+
+    this.menuAll = true;
+    this.menuCategory = false;
+  }
   public openClothes(route: string, name: string) {
     const newRoute = name.replace(/ /g, '_').toLowerCase();
     this.router.navigate(['/men/' + route+'/' +newRoute], {queryParamsHandling: 'merge'}).then();
