817aaefa3169d4f0a61be06fd4667d78c87c7675
Max Belousov <macss.belousov699@gmail.com>
2024-02-23 15:28:23 +0200

    updated slider-news

--- src/app/modules/home/@shared/components/banner/banner.component.ts
+++ src/app/modules/home/@shared/components/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, HostListener, Renderer2} from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
@@ -8,8 +8,25 @@ import {Router} from "@angular/router";
 })
 export class BannerComponent {
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              private renderer: Renderer2,
+              private el: ElementRef) {}
+
+  ngOnInit(): void {
+    this.adjustFontSize();
   }
+
+  private adjustFontSize(): void {
+    const baseFontSize = 16; // ваш базовый размер шрифта
+    const windowWidth = window.innerWidth;
+    const screenWidth = window.screen.width;
+
+    const currentTextScale = windowWidth / screenWidth;
+
+    const newFontSize = `${baseFontSize * currentTextScale}px`;
+    document.body.style.fontSize = newFontSize;
+  }
+
 
   goToSection() {
     this.router.navigate(['/men'], {queryParamsHandling: 'merge'})
