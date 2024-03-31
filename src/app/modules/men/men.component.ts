c3bed6ad114ac79a5a02f4462c6d5831df0c471d
Max Belousov <macss.belousov699@gmail.com>
2024-03-31 21:49:32 +0300

    updated mobile stylw

--- src/app/modules/men/men.component.ts
+++ src/app/modules/men/men.component.ts
@@ -1,16 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
+import {storeSelectorLang} from "../../store/selectors/store.selectors";
+import {Subscription} from "rxjs";
+import {StoreInterface} from "../../store/model/store.model";
+import {Store} from "@ngrx/store";
 
 @Component({
   selector: 'app-men',
   templateUrl: './men.component.html',
   styleUrl: './men.component.scss'
 })
-export class MenComponent implements OnInit{
-
-  constructor(private translate: TranslateService) {}
+export class MenComponent implements OnInit, OnDestroy{
+private langSubscription: Subscription;
+  constructor(private translate: TranslateService,
+               private store: Store<{store: StoreInterface}>) {}
 
   ngOnInit() {
-    this.translate.use('en');
+
+    this.initializeActiveLang();
+  }
+
+  private initializeActiveLang(){
+    this.langSubscription = this.store.select(storeSelectorLang).subscribe((lang:string) =>{
+      this.translate.use(lang)
+    })
+  }
+
+  ngOnDestroy() {
+    this.langSubscription.unsubscribe();
   }
 }
