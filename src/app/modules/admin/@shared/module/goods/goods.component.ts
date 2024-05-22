e01510ac2dcb510a1215301a67873b909ffb5c44
MacsimB <macs.belousov666@gmail.com>
2024-05-22 23:56:49 +0300

    updated

--- src/app/modules/admin/@shared/module/goods/goods.component.ts
+++ src/app/modules/admin/@shared/module/goods/goods.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { StoreInterface } from '../../../../../store/model/store.model';
+import { storeSelectorClothesData } from '../../../../../store/selectors/store.selectors';
 
 @Component({
   selector: 'app-goods',
   templateUrl: './goods.component.html',
   styleUrl: './goods.component.scss'
 })
-export class GoodsComponent {
+export class GoodsComponent implements OnInit {
+  public allContent: any[] = [];
+  errorImages: boolean[] = [];
 
+  constructor(
+    private store: Store<{ store: StoreInterface }>,
+  ) { }
+
+  ngOnInit(): void {
+    this.store.pipe(select(storeSelectorClothesData)).subscribe((data) => {
+      const newArray = [data['hoodies'], data['pants'], data['shoes'], data['valentines']].reduce((acc, innerArray) => acc.concat(innerArray), []);
+      this.allContent = newArray;
+
+      this.errorImages = new Array(this.allContent.length).fill(false);
+    });
+
+  }
+
+  public handleImageError(index: number) {
+    this.errorImages[index] = true;
+  }
 }
